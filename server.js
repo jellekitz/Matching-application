afb89b7dc2fc033889a86a37c1bb9e08012b67c9
Jellekitz <jellekitz@gmail.com>
2021-02-10 19:15:46 +0100

    Front-end features toegevoegd deel 1

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const port = 3000
 
+app.use(express.static(path.join(__dirname, 'static/public')));
+
 app.get('/', (req, res) => {
-  res.send('Home')
+    res.sendFile(path.join(__dirname + '/static/public/index.html'));
 })
 
-app.get('/aboutMe', (req, res) => {
-    res.send('Over mij')
-  })
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname + '/static/public/login.html'));
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname + '/static/public/register.html'));
+})
+
+app.get('/myAccount', (req, res) => {
+    res.sendFile(path.join(__dirname + '/static/public/myAccount.html'));
+})
+
+app.get('*', function(req, res){
+    res.send('404 not found', 404);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
