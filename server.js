c6dc660e99f4954aec3a532ab08fd923e6626596
Jellekitz <jellekitz@gmail.com>
2021-02-13 21:29:18 +0100

    routes aangemaakt

--- server.js
+++ server.js
@@ -2,29 +2,23 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const port = 3000
+const router = require('./routes/routes.js')
 
-app.use(express.static(path.join(__dirname, 'static/public')));
+// ejs view engine
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/static/public/index.html'));
-})
+// static files
+app.use(express.static(path.join(__dirname, 'static/public')))
 
-app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname + '/static/public/login.html'));
-})
+// router files
+app.use('/', router)
 
-app.get('/register', (req, res) => {
-    res.sendFile(path.join(__dirname + '/static/public/register.html'));
-})
-
-app.get('/myAccount', (req, res) => {
-    res.sendFile(path.join(__dirname + '/static/public/myAccount.html'));
-})
-
+// 404 page
 app.get('*', function(req, res){
-    res.send('404 not found', 404);
+    res.send('404 not found', 404)
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Matching-application listening at http://localhost:${port}`)
 })
