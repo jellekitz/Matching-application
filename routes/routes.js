f7b1b1b78e6ab3156adec5b16d0cd84b38e1f359
Jellekitz <jellekitz@gmail.com>
2021-02-17 18:55:42 +0100

    POST register form aangemaakt

--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,29 @@
 const express = require('express')
 const router = express.Router()
+const bodyParser = require('body-parser')
+
+// urlencodedParser variable for middleware
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 router.get('/', function(req, res){
-    const user = [
-        { name: 'Jelle' }
-    ];
-    res.render('pages/index', {
-        user: user
-    });
-});
+    res.render('pages/index')
+})
+
+router.get('/register', function(req, res){
+    res.render('pages/register', {qs: req.query})
+})
+
+router.post('/register', urlencodedParser, function(req, res){
+    console.log(req.body)
+    res.render('pages/register-succes', {data: req.body})
+})
+
+router.get('/login', function(req, res){
+    res.render('pages/login')
+})
+
+router.get('/result', function(req, res){
+    res.render('pages/result')
+})
 
 module.exports = router;
